b226342c3eb530e20b20a67804cb9f06ad047bf9
Resheba <74815401+Resheba@users.noreply.github.com>
2023-07-11 14:01:46 +0300

    more env integration

--- sheetAPI/base.py
+++ sheetAPI/base.py
@@ -4,20 +4,27 @@ dotenv.load_dotenv()
 
 CREDENTIALS = json.loads(os.getenv('CREDENTIALS'))
 AUTH_USER = json.loads(os.getenv('AUTH_USER'))
+TABLE_NAME = os.getenv('TABLE_NAME')
+
+LIST_USER_NAME = os.getenv('LIST_USER_NAME')
+LIST_OPERATIONS_NAME = os.getenv('LIST_OPERATIONS_NAME')
+
+LIST_DIRECTIOINS_NAME = os.getenv('LIST_DIRECTIOINS_NAME')
+LIST_OPERATIONS_TYPES_NAME = os.getenv('LIST_OPERATIONS_TYPES_NAME')
 
 gc, user = gspread.oauth_from_dict(credentials=CREDENTIALS, authorized_user_info=AUTH_USER)
 
-SHEET = gc.open('Finance')
+SHEET = gc.open(TABLE_NAME)
 
-OPERATIONS = SHEET.worksheet('Operations')
-USERS = SHEET.worksheet('Users')
+OPERATIONS = SHEET.worksheet(LIST_OPERATIONS_NAME)
+USERS = SHEET.worksheet(LIST_USER_NAME)
 
 DATE_FORMAT = '%d.%m.%Y %H:%M:%S'
 BUTTONS_IN_OUT = ('Приход', 'Расход')
 
 
 def operation_types_load():
-    operations_types = SHEET.worksheet('_operations_types')
+    operations_types = SHEET.worksheet(LIST_OPERATIONS_TYPES_NAME)
     result_dict = dict()
     table = operations_types.get_all_values()
 
@@ -46,7 +53,7 @@ OPERATIONS_TYPES = operation_types_load()
 
 
 def direction_types_loader():
-    directions_sheet = SHEET.worksheet('_directions')
+    directions_sheet = SHEET.worksheet(LIST_DIRECTIOINS_NAME)
     directions_table = directions_sheet.get_all_values()
     values = [value[0] for value in directions_table]
     return values
